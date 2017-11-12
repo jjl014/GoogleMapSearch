05c42f92fa3c3ea7118e1c8d6a9afd1c9d714258
Jimmy Li <jimmyli921@gmail.com>
2017-11-11 18:32:02 -0800

    Create SearchListItem

--- frontend/components/map_search/search_list.jsx
+++ frontend/components/map_search/search_list.jsx
@@ -1,9 +1,13 @@
 import React from 'react';
+import SearchListItem from './search_list_item';
 
 const SearchList = () => {
   return (
     <div>
       <h1>Search List</h1>
+      <ul>
+        <SearchListItem />
+      </ul>
     </div>
   );
 };
---
+++ frontend/components/map_search/search_list_item.jsx
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const SearchListItem = () => {
+  return (
+    <div>
+      <h1>SearchListItem</h1>
+    </div>
+  );
+};
+
+export default SearchListItem;
